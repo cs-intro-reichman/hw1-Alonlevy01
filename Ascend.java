0e6cae0fd96a10592946bc1eab8a1d90918c5da5
Alon Levy <alonlevy01@gmail.com>
2024-11-06 18:52:05 +0200

    better code

--- Ascend.java
+++ Ascend.java
@@ -5,22 +5,17 @@ import java.util.Random;
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		Random rand = new Random() ;
-		int randNum = Integer.parseInt(args[0]) + 1 ;
+		Random rand = new Random();
+		int randNum = Integer.parseInt(args[0]) + 1;
 		int num1 = rand.nextInt(randNum); 
 		int num2 = rand.nextInt(randNum);
 		int num3 = rand.nextInt(randNum);
-		int lowest = Math.min(num1, num2) ; 
-		lowest = Math.min(lowest, num3) ;
-		int highest = Math.max(num1, num2) ; 
-		highest = Math.max(highest, num3) ;
-		int middle1 = Math.min(num1, num2) ;
-		int middle2 = Math.min(num1, num3) ;
-		int middle3 = Math.min(num1, num2) ;
-		int middleA = Math.max(middle1, middle2) ;
-		int middleB = Math.max(middle2, middle3) ;
-		int middle = Math.max(middleA, middleB) ; 
-		System.out.println(num1 + " " + num2 + " " + num3) ;
-		System.out.println(lowest +" " + middle + " " + highest) ;
+		int lowest = Math.min(num1, num2); 
+		lowest = Math.min(lowest, num3);
+		int highest = Math.max(num1, num2); 
+		highest = Math.max(highest, num3);
+		int middle = (num1 + num2 + num3) - (highest + lowest);
+		System.out.println(num1 + " " + num2 + " " + num3);
+		System.out.println(lowest +" " + middle + " " + highest);
 	}
 }
