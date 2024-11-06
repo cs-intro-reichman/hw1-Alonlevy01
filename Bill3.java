de3c5c7e4eed3fdea16131b9b946b5061dcd2408
Alon Levy <alonlevy01@gmail.com>
2024-11-06 13:47:09 +0200

    updated syntax

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3 {
         double sum = Double.parseDouble(args[3]) ;
         double pay = sum/3 ; 
         pay = Math.ceil(pay) ;
-        System.out.println("Dear " + a + ", " + b + ", and " + c + " pay " + pay + " Shekels each.") ;
+        System.out.println("Dear " + c + ", " + b + ", and " + a + ": pay " + pay + " Shekels each.") ;
     }    
  }
  
